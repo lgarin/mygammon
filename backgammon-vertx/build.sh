342514772aed7aec79a3f6d08cefc79017dc55fd
Lucien <lgarin@gmx.ch>
2016-08-13 20:34:43 +0200

    improved build script

--- backgammon-vertx/build.sh
+++ backgammon-vertx/build.sh
@@ -2,20 +2,25 @@
 JAVA_HOME=/c/Dev/jdk8
 DIST_DIR=//gd04b/dev/backgammon/dist
 VERSION_PATTERN="[0-9]+\.[0-9]+\.[0-9]+"
-VERSION=1.1.0
 
+CURRENT_VERSION=$(sed -rn "s/.*backgammon.client.($VERSION_PATTERN).*/\1/gp" static/board.html)
+read V_MAJOR V_MINOR V_PATCH <<<$(IFS="."; echo $CURRENT_VERSION)
+V_PATCH=$((V_PATCH + 1))
+NEW_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+
+echo "Building version $CURRENT_VERSION"
 if [ -d $DIST_DIR ]; then
   rm -r $DIST_DIR
 fi
 mkdir $DIST_DIR
-
-./ceylonb version --set $VERSION --confirm=none backgammon.shared backgammon.server backgammon.client backgammon.test
-sed -i -r "s/(backgammon.client.)$VERSION_PATTERN/\1$VERSION/g" static/board.html
 ./ceylonb compile --offline --out=$DIST_DIR/modules backgammon.shared
 ./ceylonb compile --offline --out=$DIST_DIR/modules backgammon.server
 ./ceylonb copy --offline --out=$DIST_DIR/client --js --with-dependencies backgammon.client
 cp -r static $DIST_DIR
+git tag -f -a -m "Released version $CURRENT_VERSION" $CURRENT_VERSION
 
+echo "Preparing next version $NEW_VERSION"
+./ceylonb version --set $NEW_VERSION --confirm=none backgammon.shared backgammon.server backgammon.client backgammon.test
+sed -i -r "s/(backgammon.client.)$VERSION_PATTERN/\1$NEW_VERSION/g" static/board.html
 git add .
-git commit "Released version $VERSION"
-git tag $VERSION
+git commit -m "Version bump to $VERSION"
