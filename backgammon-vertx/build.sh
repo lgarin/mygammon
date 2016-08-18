be32517080b6b390eac3c1481c28de7c7ae8cbdd
Lucien <lgarin@gmx.ch>
2016-08-18 17:15:26 +0200

    integrate build script in eclipse

--- backgammon-vertx/build.sh
+++ backgammon-vertx/build.sh
@@ -5,6 +5,10 @@ VERSION_PATTERN="[0-9]+\.[0-9]+\.[0-9]+"
 
 CURRENT_VERSION=$(sed -rn "s/.*backgammon.client.($VERSION_PATTERN).*/\1/gp" static/board.html)
 read V_MAJOR V_MINOR V_PATCH <<<$(IFS="."; echo $CURRENT_VERSION)
+if [[ -z "$V_MAJOR" || -z "$V_MINOR" || -z "$V_PATCH" ]] ; then
+  echo "Unable to detect current version"
+  exit 1
+fi 
 VERSION_DIR="$DIST_DIR/dist-$V_MAJOR.$V_MINOR.$V_PATCH"
 NEW_VERSION="$V_MAJOR.$V_MINOR.$((V_PATCH + 1))"
 
@@ -20,7 +24,7 @@ echo "Preparing next version $NEW_VERSION"
 ./ceylonb version --set $NEW_VERSION --confirm=none backgammon.shared backgammon.server backgammon.client backgammon.test
 sed -i -r "s/(backgammon.client.)$VERSION_PATTERN/\1$NEW_VERSION/g" static/board.html
 unix2dos static/board.html
-sed -i -r "s/(backgammon.server.)$VERSION_PATTERN/\1$NEW_VERSION/g" Backgammon\ Verticle.launch
+sed -i -r "s/(backgammon.server.)$VERSION_PATTERN/\1$NEW_VERSION/g" Backgammon\ Server.launch
 unix2dos Backgammon\ Verticle.launch
 git add .
 git commit -m "Version bump to $NEW_VERSION"
